e89d005ec383836e9addd2e40db8dab8aeca97a5
Lâm Chính Nghi <lam91391@gmail.com>
2025-03-20 20:42:18 +0700

    fix bug

--- lib/screen/product_screen.dart
+++ lib/screen/product_screen.dart
@@ -35,75 +35,78 @@ class ProductScreenState extends State<ProductScreen> {
       body: Column(
         mainAxisAlignment: MainAxisAlignment.start,
         children: <Widget>[
-          Align(
-            alignment: Alignment.topRight,
-            child: Padding(
-              padding: EdgeInsetsDirectional.only(top: 25, end: 25),
-              child: IconButton(
-                icon: Icon(Icons.logout),
-                onPressed: () async {
-                  await _auth.signOut();
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                      builder: (context) => LoginScreen(),
-                    ),
-                  );
-                },
-              ),
-            ),
-          ),
-          Center(
-            child: Text(
-              'Danh sách sản phẩm',
-              style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
-            ),
-          ),
           SizedBox(height: 25),
+          Padding(
+            padding: EdgeInsetsDirectional.only(start: 15, end: 15),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text(
+                  'Danh sách sản phẩm',
+                  style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold),
+                ),
+                IconButton(
+                  icon: Icon(Icons.logout),
+                  onPressed: () async {
+                    await _auth.signOut();
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => LoginScreen(),
+                      ),
+                    );
+                  },
+                ),
+              ],
+            ),
+          ),
           Expanded(
             child: ListView.builder(
               itemCount: productsList.length,
               itemBuilder: (context, index) {
                 var product = productsList[index];
-                return Card(
-                  child: ListTile(
-                    leading: Image.network(
-                      product['image'],
-                      width: 70,
-                      height: 70,
-                      fit: BoxFit.cover,
-                    ),
-                    // ignore: prefer_interpolation_to_compose_strings
-                    title: Text("Loại: " + product['type']),
-                    // ignore: prefer_interpolation_to_compose_strings
-                    subtitle: Text("Giá: " + product['price']),
+                return Padding(
+                  padding: EdgeInsets.symmetric(horizontal: 10, vertical: 5),
+                  child: Card(
+                    child: ListTile(
+                      leading: Image.network(
+                        product['image'],
+                        width: 70,
+                        height: 70,
+                        fit: BoxFit.cover,
+                      ),
+                      // ignore: prefer_interpolation_to_compose_strings
+                      title: Text("Loại: " + product['type']),
+                      // ignore: prefer_interpolation_to_compose_strings
+                      subtitle: Text("Giá: " + product['price']),
 
-                    trailing: Row(
-                      mainAxisSize: MainAxisSize.min,
-                      children: [
-                        IconButton(
-                          icon: Icon(Icons.edit, color: Colors.green),
-                          onPressed: () async {
-                            final updatedProduct = await Navigator.push(
-                              context,
-                              MaterialPageRoute(
-                                builder: (context) => UpdateProductScreen(
-                                    productId: product['docId']),
-                              ),
-                            );
+                      trailing: Row(
+                        mainAxisSize: MainAxisSize.min,
+                        children: [
+                          IconButton(
+                            icon: Icon(Icons.edit, color: Colors.green),
+                            onPressed: () async {
+                              final updatedProduct = await Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                  builder: (context) => UpdateProductScreen(
+                                      productId: product['docId']),
+                                ),
+                              );
 
-                            if (updatedProduct != null) {
-                              setState(() {
-                                // Cập nhật danh sách sản phẩm sau khi nhận dữ liệu mới từ màn hình cập nhật
-                                productsList[index] = updatedProduct;
-                              });
-                            }
-                          },
-                        ),
-                        IconButton(
-                          icon: Icon(Icons.delete, color: Colors.red),
-                          onPressed: () async {
-                              List<Map<String, dynamic>> updatedList = await ProductService().deleteProduct(
+                              if (updatedProduct != null) {
+                                setState(() {
+                                  // Cập nhật danh sách sản phẩm sau khi nhận dữ liệu mới từ màn hình cập nhật
+                                  productsList[index] = updatedProduct;
+                                });
+                              }
+                            },
+                          ),
+                          IconButton(
+                            icon: Icon(Icons.delete, color: Colors.red),
+                            onPressed: () async {
+                              List<Map<String, dynamic>> updatedList =
+                                  await ProductService().deleteProduct(
                                 product['docId'],
                                 index,
                                 context,
@@ -112,9 +115,10 @@ class ProductScreenState extends State<ProductScreen> {
                               setState(() {
                                 productsList = updatedList;
                               });
-                          },
-                        ),
-                      ],
+                            },
+                          ),
+                        ],
+                      ),
                     ),
                   ),
                 );
@@ -145,54 +149,4 @@ class ProductScreenState extends State<ProductScreen> {
       });
     }
   }
-
-  // Xóa sản phẩm
-  // Future<void> deleteProduct(String docId, int index) async {
-  //   try {
-  //     showDialog(
-  //       context: context,
-  //       builder: (BuildContext context) {
-  //         return AlertDialog(
-  //           title: Text('Xác nhận xóa'),
-  //           content: Text('Bạn có chắc chắn muốn xóa sản phẩm này không?'),
-  //           actions: <Widget>[
-  //             TextButton(
-  //               onPressed: () {
-  //                 Navigator.of(context).pop();
-  //               },
-  //               child: Text('Hủy'),
-  //             ),
-  //             TextButton(
-  //               onPressed: () {
-  //                 FirebaseFirestore.instance
-  //                     .collection('products')
-  //                     .doc(docId)
-  //                     .delete();
-
-  //                 setState(() {
-  //                   productsList.removeAt(index);
-  //                 });
-
-  //                 ScaffoldMessenger.of(context).showSnackBar(
-  //                   SnackBar(content: Text('Đã xóa sản phẩm thành công!')),
-  //                 );
-
-  //                 Navigator.of(context).pop();
-  //               },
-  //               child: Text(
-  //                 'Xóa',
-  //                 style: TextStyle(color: Colors.red),
-  //               ),
-  //             ),
-  //           ],
-  //         );
-  //       },
-  //     );
-  //   } catch (e) {
-  //     print("Lỗi khi xóa: $e");
-  //     ScaffoldMessenger.of(context).showSnackBar(
-  //       SnackBar(content: Text('Xóa sản phẩm thất bại!')),
-  //     );
-  //   }
-  // }
 }
